cbdd252a54bda58f726fc4dfa5c95a3363638d5f
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-04-25 21:17:49 -0500

    added middleware to server.js and created new js files the adding and reading of notes in the db folder

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express= require ('express');
 const path= require ('path');
 const api= require ('./public/assets/index.js')
 
-const PORT = process.env.port || 3001
-
+const PORT = process.env.port || 3001;
 const app = express();
+
+// We will need middleware for parsing JSON 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
+app.use(express.static('public'));
+// We will need to define our routes
+
+
+// We will need to start our server
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
